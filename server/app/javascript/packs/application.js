77140506040a9567ab25e07ac0c825b09c456946
Matheus Motta <matheusmottaq@gmail.com>
2020-11-30 18:18:28 -0300

    rubocop config

---
+++ server/app/javascript/packs/application.js
@@ -0,0 +1,6 @@
+/* eslint-disable quotes */
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+// eslint-disable-next-line import/no-unresolved
+require("channels")
