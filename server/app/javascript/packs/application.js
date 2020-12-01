010bd513befb809afa730aaea5ab5669c1ac9701
Matheus Motta <matheusmottaq@gmail.com>
2020-12-01 12:57:25 -0300

    server criado

--- server/app/javascript/packs/application.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable quotes */
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-// eslint-disable-next-line import/no-unresolved
-require("channels")
